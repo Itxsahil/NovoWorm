ea98293a5a8d2cf09cc88bccbc095f4450b2f2c6
itxsahil <sahilkh9087@gmail.com>
2024-11-19 20:40:11 +0530

    add Novel in catagory

--- src/models/Book.model.js
+++ src/models/Book.model.js
@@ -17,7 +17,7 @@ const bookSchema = new mongoose.Schema({
   },
   categories: {
     type: String,
-    enum: ['Category 1', 'Category 2', 'Category 3'],
+    enum: ['Category 1', 'Category 2', 'Category 3', 'Novel'],
     required: true,
   },
   status: {
